87c5c6131b3648f9a796f3958f6aa1fba96daba0
Dr. Walter W <75395678+drwalterwhite@users.noreply.github.com>
2020-12-06 01:13:24 -0300

    Add files via upload

--- dist/assets/js/vendors/charts/_versus.js
+++ dist/assets/js/vendors/charts/_versus.js
@@ -93,7 +93,7 @@ function GetVersus(url, pl1, pl2, bg) {
 					},
 					feature: {
 						saveAsImage: {
-							name: p1Data[0].name + '-x-' + p2Data[0].name + '-Ao-Vivo-Online-No-Multicanais',
+							name: p1Data[0].name + '-x-' + p2Data[0].name + '-Ao-Vivo-Online',
 							show: true,
 							pixelRatio: 1,
 							icon: 'image://' + SaveIcon + ''
